883ec26fe3972a332da893d28219421719f30fe5
aravind <aravindersuram@gmail.com>
2020-04-27 17:06:42 +0530

    present or abs

--- empwage.java
+++ empwage.java
@@ -4,7 +4,7 @@ public class empwage{
 		int IS_PRESENT=1;
 		
 		double empCheck=Math.floor(Math.random() * 10 ) %2;
-		System.out.println("Welcome to the Employee Use Case problem");
+		System.out.println("Welcome to EmployeeWage Computation");
 
 		if (empCheck==IS_PRESENT){
 			System.out.println("Employee is present");
