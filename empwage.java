65e9bcde5a8658b5020b94cf0175c756c6838c1d
aravind <aravindersuram@gmail.com>
2020-04-27 17:10:02 +0530

    uc2

--- empwage.java
+++ empwage.java
@@ -2,12 +2,17 @@ public class empwage{
 	
 	public static void main(String[] args){
 		int IS_PRESENT=1;
+                 double wagePerHour=20;
+		double fullDay=8;	
+		double empWage;
 		
 		double empCheck=Math.floor(Math.random() * 10 ) %2;
 		System.out.println("Welcome to the Employee Use Case problem");
 
 		if (empCheck==IS_PRESENT){
 			System.out.println("Employee is present");
+                        empWage=wagePerHour*fullDay;
+			System.out.println("Employee wage is: "+ empWage);
 		}
 		else{
 			System.out.println("Employee is absent");
